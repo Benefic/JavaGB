9e031b61a8faf6c4d2bf057e6bd6c9b0a5c5cc3a
benefic <abenefic@gmail.com>
2020-08-18 21:24:39 +0300

    Homework lesson 1: try 1

---
+++ src/main/java/lesson1/CheckLeapYearTask.java
@@ -0,0 +1,27 @@
+package lesson1;
+
+public class CheckLeapYearTask {
+    public static void main(String[] args) {
+        isLeap(1600);
+        isLeap(2000);
+        isLeap(1700);
+        isLeap(1704);
+        isLeap(2024);
+        isLeap(2021);
+        isLeap(0);
+    }
+
+    private static void isLeap(int year) {
+        if (year == 0) {
+            System.out.println("Year " + year + " IS NOT leap! (actually, doesn't exist)");
+        } else if (year % 400 == 0) {
+            System.out.println("Year " + year + " IS leap!");
+        } else if (year % 100 == 0) {
+            System.out.println("Year " + year + " IS NOT leap!");
+        } else if (year % 4 == 0) {
+            System.out.println("Year " + year + " IS leap!");
+        } else {
+            System.out.println("Year " + year + " IS NOT leap!");
+        }
+    }
+}
---
+++ src/main/java/lesson1/PrintHelloTask.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class PrintHelloTask {
+    public static void main(String[] args) {
+        printHello("Василий Петрович");
+        printHello("Игорь");
+        printHello("Мадест");
+        printHello(null);
+    }
+
+    private static void printHello(String name){
+        System.out.println("Привет, " + (name == null ? "Мир":name) + "!");
+    }
+}
