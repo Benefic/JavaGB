fa592c78301ac463958b8bf6f2ec2ce25233f1ad
Benefic <abenefic@gmail.com>
2020-08-24 10:53:55 +0300

    homework 2; tasks 1-6

---
+++ src/main/java/lesson2/Task3.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+
+        Task1.printIntArray(array);
+    }
+}
---
+++ src/main/java/lesson2/Task7.java
@@ -0,0 +1,28 @@
+package lesson2;
+
+public class Task7 {
+    public static void main(String[] args) {
+
+    }
+
+    private static void move(int[] array, int side) {
+
+        // Что-то не даётся, не проверяйте...
+
+//        int tmpSrc;
+//        int tmpDest;
+//
+//        for (int i = 0; i < Math.abs(side); i++) {
+//            for (int j = 0; j < array.length; j++) {
+//                int pos = j + side;
+//                if (pos > array.length){
+//                    pos -= array.length;
+//                }
+//                tmpSrc = array[j];
+//                tmpDest = array[j+pos];
+//                array[j+pos] = array[j];
+//                tmpDest = tmpSrc;
+//            }
+//        }
+    }
+}
