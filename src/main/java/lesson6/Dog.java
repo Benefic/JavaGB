ae4d712022ee18452d6ba6a89fd055f5330b8cc4
benefic <abenefic@gmail.com>
2020-09-05 15:19:06 +0300

    Homework lesson 6: try 1

---
+++ src/main/java/lesson6/Dog.java
@@ -0,0 +1,32 @@
+package lesson6;
+
+public class Dog extends Animal {
+
+    private static final float DEFAULT_MAX_JUMP = 0.5f;
+    private static final int DEFAULT_MAX_RUN = 500;
+    private static final int DEFAULT_MAX_SWIM = 10;
+
+    public Dog(String name) {
+        this(name, DEFAULT_MAX_RUN, DEFAULT_MAX_JUMP, DEFAULT_MAX_SWIM);
+    }
+
+    public Dog(String name, int maxRunDistance) {
+        this(name, maxRunDistance, DEFAULT_MAX_JUMP, DEFAULT_MAX_SWIM);
+    }
+
+    public Dog(String name, int maxRunDistance, float maxJumpHeight) {
+        this(name, maxRunDistance, maxJumpHeight, DEFAULT_MAX_SWIM);
+    }
+
+    public Dog(String name, int maxRunDistance, float maxJumpHeight, int maxSwimDistance) {
+        this.name = name;
+        this.maxRun = maxRunDistance;
+        this.maxJump = maxJumpHeight;
+        this.maxSwim = maxSwimDistance;
+    }
+
+    @Override
+    public String toString() {
+        return "Dod " + name;
+    }
+}
---
+++ src/main/java/lesson6/Test.java
@@ -0,0 +1,36 @@
+package lesson6;
+
+import java.util.Random;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        Animal[] animals = new Animal[7]; // в сумме у нас 7 конструкторов, проверим все. Хотя их могло быть и больше...
+
+        // в цикле не делаю, ибо разные контсрукторы надо проверить
+
+        animals[0] = new Cat("Barsik");
+        animals[1] = new Cat("Vaska", 100);
+        animals[2] = new Cat("Boris", 300, 2.5f);
+
+        animals[3] = new Dog("Tuzik");
+        animals[4] = new Dog("Bobik", 400);
+        animals[5] = new Dog("Rex", 600, 1);
+        animals[6] = new Dog("Sharik", 500, 0.5f, 15);
+
+        Random random = new Random();
+        int testCount = 3;
+        for (int i = 0; i < testCount; i++) {
+            for (Animal animal : animals) {
+                animal.run(random.nextInt(10) * 100);
+                animal.swim(random.nextInt(3) * 10);
+                animal.jump(random.nextFloat() * 5);
+                System.out.println("---------------------");
+            }
+            System.out.println("+++++++++++++++++++++");
+        }
+
+    }
+}
