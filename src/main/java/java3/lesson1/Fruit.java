df395ad0a7f19ed46a6866586edbd4cf2f2e4e66
ALEKSANDR KOYNOSOV <abenefic@gmail.com>
2020-10-21 21:54:15 +0300

    Homework lesson 1: try 1

---
+++ src/main/java/java3/lesson1/Fruit.java
@@ -0,0 +1,7 @@
+package java3.lesson1;
+
+public class Fruit {
+    public float getWeight() {
+        return 0;
+    }
+}
---
+++ src/main/java/java3/lesson1/Orange.java
@@ -0,0 +1,7 @@
+package java3.lesson1;
+
+public class Orange extends Fruit {
+    public float getWeight() {
+        return 1.5f;
+    }
+}
