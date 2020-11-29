13bc58f8d67b51eb3b7960e04828916de465f54a
ALEKSANDR KOYNOSOV <abenefic@gmail.com>
2020-11-29 20:42:44 +0300

    algorithms lesson3 Deque

--- src/main/java/algorithms/lesson3/Main.java
+++ src/main/java/algorithms/lesson3/Main.java
@@ -1,9 +1,38 @@
 package algorithms.lesson3;
 
-import java.util.Comparator;
-
 public class Main {
     public static void main(String[] args) {
+
+        MyDeque<Integer> deque = new MyDeque<>(5);
+
+        deque.insertRight(1);
+        deque.insertRight(2);
+        deque.insertRight(3);
+        deque.insertRight(31);
+        deque.insertRight(32);
+        deque.insertRight(33);
+        System.out.println(deque);
+
+        deque.insertLeft(4);
+        System.out.println(deque);
+
+        deque.insertLeft(5);
+        System.out.println(deque);
+
+        System.out.println(deque.removeLeft());
+        System.out.println(deque);
+
+        System.out.println(deque.removeRight());
+        System.out.println(deque);
+
+        deque.insertRight(6);
+        System.out.println(deque);
+
+        deque.insertLeft(7);
+        System.out.println(deque);
+
+        deque.insertLeft(8);
+        System.out.println(deque);
 //        MyStack<Integer> stack = new MyStack<>();
 //        stack.push(1);
 //        stack.push(2);
@@ -36,14 +65,14 @@ public class Main {
 //        System.out.println(queue);
 //        queue.insert(2);
 
-        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>(Comparator.reverseOrder());
-        mpq.insert(5);
-        mpq.insert(2);
-        mpq.insert(15);
-        mpq.insert(3);
-
-        for (int i = 0; i < 4; i++) {
-            System.out.println(mpq.remove());
-        }
+//        MyPriorityQueue<Integer> mpq = new MyPriorityQueue<>(Comparator.reverseOrder());
+//        mpq.insert(5);
+//        mpq.insert(2);
+//        mpq.insert(15);
+//        mpq.insert(3);
+//
+//        for (int i = 0; i < 4; i++) {
+//            System.out.println(mpq.remove());
+//        }
     }
 }
---
+++ src/main/java/algorithms/lesson3/MyDeque.java
@@ -0,0 +1,115 @@
+package algorithms.lesson3;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class MyDeque<T> {
+
+    private final int DEFAULT_CAPACITY = 10;
+    private T[] list;
+    private int size;
+    private int begin;
+    private int end;
+
+
+    public MyDeque(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+        list = (T[]) new Object[capacity];
+    }
+
+    public MyDeque() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public T peekRight() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list[end - 1];
+    }
+
+    public T peekLeft() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return list[begin];
+    }
+
+    public T removeRight() {
+        T temp = peekRight();
+        size--;
+        end = nextLeftIndex(end);
+        list[end] = null;
+        return temp;
+    }
+
+    public T removeLeft() {
+        T temp = peekLeft();
+        size--;
+        list[begin] = null;
+        begin = nextRightIndex(begin);
+        return temp;
+    }
+
+    public void insertRight(T item) {
+        if (isFull()) {
+            reCapacity(size + size / 2);
+            //throw new IllegalStateException();
+        }
+        size++;
+        list[end] = item;
+        end = nextRightIndex(end);
+    }
+
+    public void insertLeft(T item) {
+        if (isFull()) {
+            reCapacity(size + size / 2);
+//            throw new IllegalStateException();
+        }
+        size++;
+        begin = nextLeftIndex(begin);
+        list[begin] = item;
+    }
+
+    private int nextRightIndex(int index) {
+        return (index + 1) % list.length;
+    }
+
+    private int nextLeftIndex(int index) {
+        return index == 0 ? list.length - 1 : --index;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == list.length;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(list) + " begin: " + begin + " end: " + end;
+    }
+
+    private void reCapacity(int newCapacity) {
+        T[] temp = (T[]) new Object[newCapacity];
+        // end == begin is always true for full deque
+        if (begin == 0) {
+            System.arraycopy(list, begin, temp, 0, size);
+        } else {
+            System.arraycopy(list, begin, temp, 0, list.length - begin);
+            System.arraycopy(list, 0, temp, list.length - begin, end);
+        }
+        list = temp;
+        begin = 0;
+        end = size;
+    }
+
+}
